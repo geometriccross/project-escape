e8dd6474f9d4358ed5237a2584778864dca54966
Desktop <kurosu.0122.playstation@gmail.com>
2023-12-01 22:52:01 +0900

    layerの移動を制御するscriptを大幅に変更

--- src/movement/user_move.gd
+++ src/movement/user_move.gd
@@ -1,11 +1,27 @@
-extends Node3D
+extends CharacterBody3D
 
-@export var x_speed: float = 1.0
-@export var y_speed: float = 1.0
 
-func _input(event):
-	var left_right: float = int(event.is_action("ui_left")) * -1 + int(event.is_action("ui_right"))
-	self.move(x_speed * left_right, y_speed * float(event.is_action("ui_up")))
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
-func move(x: float, y: float) -> void:
-	self.position += Vector3(x, y, self.position.z)
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
