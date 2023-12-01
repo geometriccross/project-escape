e898d134e02c8556fe66bba79238958229cc94ab
Desktop <kurosu.0122.playstation@gmail.com>
2023-12-01 23:25:19 +0900

    視点変更のスクリプトを作成

---
+++ src/movement/user_sight_move.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+@export var sensitivity: float = 0.01
+
+# アキュムレータ
+var rotation_x = 0
+var rotation_y = 0
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton: 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event.is_action_pressed("ui_cancel"): 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	sight_rotate(event, Input.mouse_mode, $Neck/Camera3D, sensitivity)
+
+func sight_rotate(event: InputEvent, mouse_mode: int, camera: Camera3D, sensitivity: float) -> void:
+	if event is InputEventMouseMotion and mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotation_x -= event.relative.x * sensitivity
+		rotation_y -= event.relative.y * sensitivity
+		camera.transform.basis = Basis()
+		
+		camera.rotate_object_local(Vector3(0, 1, 0), rotation_x)
+		camera.rotate_object_local(Vector3(1, 0, 0), rotation_y)
